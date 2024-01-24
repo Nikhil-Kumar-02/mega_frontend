cb6611ad591c4e3b3df26c79c3c9d032a01abdb1
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-01-24 07:35:37 +0530

    added logic to change to green color on various password vaildation checks

--- src/components/core/Auth/ChooseNewPassword.jsx
+++ src/components/core/Auth/ChooseNewPassword.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { FaLongArrowAltLeft } from "react-icons/fa";
 import './ChooseNewPassword.css'
 import ButtonComponent from "../home/buttonComponent";
@@ -8,6 +8,41 @@ import PasswordInputComponent from '../../common/PasswordInputComponent'
 
 const ChooseNewPassword = (props) => {
 
+  const [validity , setValidity] = useState({lowercase:false , uppercase:false , special:false , 
+                                              number:false , size : false});
+
+    function changeHandler(e){
+      const currentPassword = e.target.value;
+
+      setValidity((prev) => {
+        const updatedValidity = {...prev};
+        for(let i=0;i<currentPassword.length;i++){
+          const newInput = currentPassword[i];
+          if(newInput>='0' && newInput<='9'){
+            updatedValidity.number = true    
+          }
+          else if(newInput>='a' && newInput<='z'){
+            updatedValidity.lowercase = true
+          }
+          else if(newInput>='A' && newInput<='Z'){
+            updatedValidity.uppercase = true
+          }
+          else{
+            updatedValidity.special = true
+          }
+        }
+       
+        if(currentPassword.length>=8){
+          updatedValidity.size = true
+        }
+        else{
+          updatedValidity.size = false
+        }
+        return updatedValidity;
+      });
+
+    }
+
   return (
     <div className="chooseNewPassword_wrapper">
       <div className="ChooseNewPassword">
@@ -15,21 +50,32 @@ const ChooseNewPassword = (props) => {
         <p>
             Almost done. Enter your new password and you are all set.
         </p>
-
-        <PasswordInputComponent title={"New Password"} placeholder={"Enter New Password"}>
-        </PasswordInputComponent>
+        <div onChange={changeHandler}>
+          <PasswordInputComponent title={"New Password"} placeholder={"Enter New Password"}>
+          </PasswordInputComponent>
+        </div>
 
         <PasswordInputComponent title={"Confirm New Password"} placeholder={"Confirm Above Password"}></PasswordInputComponent>
 
         <div className="password_validator">
             <div>
-                <div><FaCheckCircle />one lowercase character</div>
-                <div><FaCheckCircle />one uppercase character</div>
-                <div><FaCheckCircle />one number</div>
+                <div className={validity.lowercase ? "greenCheck" : ""}>
+                  <FaCheckCircle />one lowercase character
+                </div>
+                <div className={validity.uppercase ? "greenCheck" : ""}>
+                  <FaCheckCircle />one uppercase character
+                </div>
+                <div className={validity.number ? "greenCheck" : ""}>
+                  <FaCheckCircle />one number
+                </div>
             </div>
             <div>
-                <div><FaCheckCircle />one special character</div>
-                <div><FaCheckCircle />8 character minimum</div>
+                <div className={validity.special ? "greenCheck" : ""}>
+                  <FaCheckCircle />one special character
+                </div>
+                <div className={validity.size ? "greenCheck" : ""}>
+                  <FaCheckCircle />8 character minimum
+                </div>
             </div>
         </div>
 
