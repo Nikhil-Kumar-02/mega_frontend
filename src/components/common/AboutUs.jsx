58afa125973f73b98cce3122e9c904a2a320db0f
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-02-01 07:43:02 +0530

    added section 2 of about us page

--- src/components/common/AboutUs.jsx
+++ src/components/common/AboutUs.jsx
@@ -4,6 +4,8 @@ import HilightedText from "../core/home/hilightedText";
 import aboutUs1 from '../../assets/aboutUs1.png'
 import aboutUs2 from '../../assets/aboutUs2.png'
 import aboutUs3 from '../../assets/aboutUs3.png'
+import { RiDoubleQuotesL } from "react-icons/ri";
+import { RiDoubleQuotesR } from "react-icons/ri";
 
 const AboutUs = (props) => {
   return (
@@ -14,11 +16,20 @@ const AboutUs = (props) => {
             <HilightedText>Brighter Future</HilightedText>
             <p>Studynotion is at the forefront of driving innovation in online education. We're passionate about creating a brighter future by offering cutting-edge courses, leveraging emerging technologies, and nurturing a vibrant learning community.</p>
             <div className="aboutUs_section1_images">
-                <img src={aboutUs1} height={280}></img>
-                <img src={aboutUs2} height={280}></img>
-                <img src={aboutUs3} height={280}></img>
+                <img src={aboutUs1} height={280} alt=""></img>
+                <img src={aboutUs2} height={280} alt=""></img>
+                <img src={aboutUs3} height={280} alt=""></img>
             </div>
         </div>
+
+        <div className="aboutUs_section2">
+            <h1>
+            <RiDoubleQuotesL color="gray"></RiDoubleQuotesL>
+            We are passionate about revolutionizing the way we learn. Our innovative platform <HilightedText> combines technology</HilightedText>, <span> expertise</span>, and community to create an <span>unparalleled educational experience.</span>
+            <RiDoubleQuotesR color="gray"></RiDoubleQuotesR>
+            </h1>
+        </div>
+
       
     </div>
   )
