740b9ba60b10b74f9d11be62ceba7773a109a5df
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-02-01 10:38:39 +0530

    added section 3 and 4 of about us page

--- src/components/common/AboutUs.jsx
+++ src/components/common/AboutUs.jsx
@@ -4,6 +4,7 @@ import HilightedText from "../core/home/hilightedText";
 import aboutUs1 from '../../assets/aboutUs1.png'
 import aboutUs2 from '../../assets/aboutUs2.png'
 import aboutUs3 from '../../assets/aboutUs3.png'
+import aboutUs4 from '../../assets/aboutUs4.png'
 import { RiDoubleQuotesL } from "react-icons/ri";
 import { RiDoubleQuotesR } from "react-icons/ri";
 
@@ -30,6 +31,56 @@ const AboutUs = (props) => {
             </h1>
         </div>
 
+        <div className="aboutUs_section3">
+            <div className="aboutUs_section3_firstBlock">
+                <div>
+                    <h1>Our Founding Story</h1>
+                    <p>                
+                        Our e-learning platform was born out of a shared vision and passion for transforming education. It all began with a group of educators, technologists, and lifelong learners who recognized the need for accessible, flexible, and high-quality learning opportunities in a rapidly evolving digital world.
+                    </p>
+                    <br></br>
+                    <p>
+                        As experienced educators ourselves, we witnessed firsthand the limitations and challenges of traditional education systems. We believed that education should not be confined to the walls of a classroom or restricted by geographical boundaries. We envisioned a platform that could bridge these gaps and empower individuals from all walks of life to unlock their full potential.
+                    </p>
+                </div>
+                <div>
+                    <img src={aboutUs4} alt="" height={280}></img>
+                </div>
+            </div>
+            <div className="aboutUs_section3_secondBlock">
+                <div>
+                    <h1>Our Vision</h1>
+                    <p>
+                        With this vision in mind, we set out on a journey to create an e- learning platform that would revolutionize the way people learn. Our team of dedicated experts worked tirelessly to develop a robust and intuitive platform that combines cutting-edge technology with engaging content, fostering a dynamic and interactive learning experience.
+                    </p>
+                </div>
+                <div>
+                    <h1>Our Mission</h1>
+                    <p>
+                        Our mission goes beyond just delivering courses online. We wanted to create a vibrant community of learners, where individuals can connect, collaborate, and learn from one another. We believe that knowledge thrives in an environment of sharing and dialogue, and we foster this spirit of collaboration through forums, live sessions, and networking opportunities.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div className="aboutUs_section4">
+            <div>
+                <h1>5K</h1>
+                <p>Active Students</p>
+            </div>
+            <div>
+                <h1>10+</h1>
+                <p>Mentors</p>
+            </div>
+            <div>
+                <h1>200+</h1>
+                <p>Courses</p>
+            </div>
+            <div>
+                <h1>50+</h1>
+                <p>Awards</p>
+            </div>
+        </div>
       
     </div>
   )
